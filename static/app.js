5d907d945e8d59e7fffe2c7c7428629694eb6abc
Shaik Ikbhal Basha <iqbalforall@gmail.com>
2023-07-23 13:08:31 +0530

    coomit3

--- static/app.js
+++ static/app.js
@@ -64,42 +64,18 @@ Vue.component('todo-list', {
 });
 
 Vue.component('board', {
+  props: ['boardData'],
   data() {
     return {
-      boardName: 'My Board',
-      lists: [
-        {
-          listName: 'List 1',
-          todos: [
-            { text: 'Todo 1 for List 1', completed: false },
-            { text: 'Todo 2 for List 1', completed: true },
-            { text: 'Todo 3 for List 1', completed: false },
-          ],
-        },
-        {
-          listName: 'List 2',
-          todos: [
-            { text: 'Todo 1 for List 2', completed: true },
-            { text: 'Todo 2 for List 2', completed: false },
-            { text: 'Todo 3 for List 2', completed: true },
-          ],
-        },
-        {
-          listName: 'List 3',
-          todos: [
-            { text: 'Todo 1 for List 3', completed: false },
-            { text: 'Todo 2 for List 3', completed: false },
-            { text: 'Todo 3 for List 3', completed: true },
-          ],
-        },
-      ],
       newListName: '',
+      editingBoardName: false,
+      editingListIndex: -1,
     };
   },
   methods: {
     addList() {
       if (this.newListName) {
-        this.lists.push({
+        this.boardData.lists.push({
           listName: this.newListName,
           todos: [],
         });
@@ -107,69 +83,57 @@ Vue.component('board', {
       }
     },
     deleteList(index) {
-      this.lists.splice(index, 1);
+      this.boardData.lists.splice(index, 1);
     },
-    saveBoard() {
-      const boardData = {
-        boardName: this.boardName,
-        lists: this.lists,
-      };
-      localStorage.setItem('boardData', JSON.stringify(boardData));
-      alert('Board saved successfully!');
+    startEditingBoardName() {
+      this.editingBoardName = true;
     },
-    exportBoard() {
-      const boardData = {
-        boardName: this.boardName,
-        lists: this.lists,
-      };
-      const jsonData = JSON.stringify(boardData, null, 2);
-      const blob = new Blob([jsonData], { type: 'application/json' });
-      const url = URL.createObjectURL(blob);
-      const a = document.createElement('a');
-      a.href = url;
-      a.download = 'board_data.json';
-      a.click();
-      URL.revokeObjectURL(url);
+    saveBoardName() {
+      this.editingBoardName = false;
     },
-  },
-  created() {
-    const savedData = localStorage.getItem('boardData');
-    if (savedData) {
-      const parsedData = JSON.parse(savedData);
-      this.boardName = parsedData.boardName;
-      this.lists = parsedData.lists;
-      alert('Board data loaded from local storage.');
-    }
+    startEditingListName(index) {
+      this.editingListIndex = index;
+    },
+    saveListName() {
+      this.editingListIndex = -1;
+    },
   },
   template: `
     <div>
-      <h1 class="text-3xl font-bold mb-4">{{ boardName }}</h1>
-      <div class="flex mb-4">
+      <h1 class="text-3xl font-bold mb-4" v-if="!editingBoardName" @click="startEditingBoardName">{{ boardData.boardName }}</h1>
+      <input v-if="editingBoardName" v-model="boardData.boardName" @keyup.enter="saveBoardName" @blur="saveBoardName" class="text-3xl font-bold mb-4 outline-none border-b border-blue-500">
+      <div class="mb-4 flex">
         <input
           type="text"
+          v-if="!editingListIndex"
           v-model="newListName"
           @keyup.enter="addList"
           placeholder="Enter new list name"
           class="px-4 py-2 border rounded-lg"
         >
-        <button @click="addList" class="px-4 py-2 ml-2 bg-blue-500 text-white rounded-lg">
+        <button v-if="!editingListIndex" @click="addList" class="px-4 py-2 ml-2 bg-blue-500 text-white rounded-lg">
           Add List
         </button>
-        <button @click="saveBoard" class="px-4 py-2 ml-2 bg-green-500 text-white rounded-lg">
-          Save Board
-        </button>
-        <button @click="exportBoard" class="px-4 py-2 ml-2 bg-yellow-500 text-white rounded-lg">
-          Export Board
+        <input
+          v-if="editingListIndex === index"
+          v-model="boardData.lists[index].listName"
+          @keyup.enter="saveListName"
+          @blur="saveListName"
+          class="px-4 py-2 border rounded-lg"
+        >
+        <button v-if="editingListIndex === index" @click="saveListName" class="px-4 py-2 ml-2 bg-blue-500 text-white rounded-lg">
+          Save
         </button>
       </div>
       
       <div class="flex space-x-4">
         <todo-list
-          v-for="(list, index) in lists"
+          v-for="(list, index) in boardData.lists"
           :key="index"
           :listName="list.listName"
           :todos="list.todos"
           @delete-list="deleteList(index)"
+          @start-editing="startEditingListName(index)"
         ></todo-list>
       </div>
       
@@ -177,11 +141,146 @@ Vue.component('board', {
   `,
 });
 
-new Vue({
-  el: '#app',
+Vue.component('workspace', {
+  data() {
+    return {
+      drawerOpen: false,
+      newBoardName: '',
+      boards: [
+        {
+          boardName: 'Board 1',
+          lists: [
+            {
+              listName: 'List 1',
+              todos: [
+                { text: 'Todo 1 for List 1 in Board 1', completed: false },
+                { text: 'Todo 2 for List 1 in Board 1', completed: true },
+                { text: 'Todo 3 for List 1 in Board 1', completed: false },
+              ],
+            },
+            {
+              listName: 'List 2',
+              todos: [
+                { text: 'Todo 1 for List 2 in Board 1', completed: true },
+                { text: 'Todo 2 for List 2 in Board 1', completed: false },
+                { text: 'Todo 3 for List 2 in Board 1', completed: true },
+              ],
+            },
+          ],
+        },
+        {
+          boardName: 'Board 2',
+          lists: [
+            {
+              listName: 'List 1',
+              todos: [
+                { text: 'Todo 1 for List 1 in Board 2', completed: false },
+                { text: 'Todo 2 for List 1 in Board 2', completed: true },
+                { text: 'Todo 3 for List 1 in Board 2', completed: false },
+              ],
+            },
+            {
+              listName: 'List 2',
+              todos: [
+                { text: 'Todo 1 for List 2 in Board 2', completed: true },
+                { text: 'Todo 2 for List 2 in Board 2', completed: false },
+                { text: 'Todo 3 for List 2 in Board 2', completed: true },
+              ],
+            },
+          ],
+        },
+      ],
+    };
+  },
+  methods: {
+    toggleDrawer() {
+      this.drawerOpen = !this.drawerOpen;
+    },
+    switchBoard(boardIndex) {
+      this.drawerOpen = false;
+      this.$emit('switch-board', boardIndex);
+    },
+    addBoard() {
+      if (this.newBoardName) {
+        this.boards.push({
+          boardName: this.newBoardName,
+          lists: [],
+        });
+        this.newBoardName = '';
+      }
+    },
+    saveBoards() {
+      localStorage.setItem('boards', JSON.stringify(this.boards));
+      alert('Boards saved successfully!');
+    },
+    exportBoards() {
+      const data = JSON.stringify(this.boards);
+      const blob = new Blob([data], { type: 'application/json' });
+      const url = URL.createObjectURL(blob);
+      const a = document.createElement('a');
+      a.href = url;
+      a.download = 'boards.json';
+      a.click();
+      URL.revokeObjectURL(url);
+    },
+  },
   template: `
-    <div class="container mx-auto px-4 py-8">
-      <board></board>
+    <div class="flex">
+      <div v-if="drawerOpen" class="fixed inset-y-0 left-0 z-10 w-64 bg-white shadow-lg">
+        <div class="p-4 border-b">
+          <button @click="toggleDrawer" class="text-gray-600">
+            <i class="fas fa-times"></i>
+          </button>
+        </div>
+        <div class="p-4">
+          <h2 class="text-xl font-bold mb-2">Boards</h2>
+          <ul>
+            <li v-for="(board, index) in boards" :key="index" @click="switchBoard(index)" class="cursor-pointer text-blue-500 hover:underline">
+              {{ board.boardName }}
+            </li>
+          </ul>
+        </div>
+        <div class="p-4">
+          <input
+            type="text"
+            v-model="newBoardName"
+            @keyup.enter="addBoard"
+            placeholder="Enter new board name"
+            class="px-4 py-2 border rounded-lg"
+          >
+          <button @click="addBoard" class="px-4 py-2 ml-2 bg-blue-500 text-white rounded-lg">
+            Add Board
+          </button>
+        </div>
+        <div class="p-4">
+          <button @click="saveBoards" class="px-4 py-2 bg-blue-500 text-white rounded-lg">
+            Save
+          </button>
+          <button @click="exportBoards" class="px-4 py-2 ml-2 bg-green-500 text-white rounded-lg">
+            Export
+          </button>
+        </div>
+      </div>
+      <div class="flex-1">
+        <div class="p-4">
+          <button @click="toggleDrawer" class="text-gray-600">
+            <i class="fas fa-bars"></i>
+          </button>
+        </div>
+        <div class="container mx-auto px-4 py-8">
+          <board :boardData="boards[0]" @delete-list="deleteList(index)"></board>
+        </div>
+      </div>
     </div>
   `,
 });
+
+new Vue({
+  el: '#app',
+  template: `
+    <div>
+      <workspace></workspace>
+    </div>
+  `,
+});
+
