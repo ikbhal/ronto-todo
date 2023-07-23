28a320eea6060509e39047d5e67479c1ec506af8
Shaik Ikbhal Basha <iqbalforall@gmail.com>
2023-07-23 12:05:40 +0530

    cmponent

--- static/app.js
+++ static/app.js
@@ -1,5 +1,4 @@
-new Vue({
-  el: '#app',
+Vue.component('todo-list', {
   data() {
     return {
       todos: [],
@@ -35,4 +34,30 @@ new Vue({
       deep: true,
     },
   },
+  template: `
+    <div class="container mx-auto px-4 py-8">
+      <h1 class="text-3xl font-bold mb-4">Todo App</h1>
+      <div class="mb-4 flex items-center">
+        <input type="text" v-model="newTodo" @keyup.enter="addTodo" placeholder="Enter new todo" class="flex-1 px-4 py-2 border rounded-lg">
+        <button @click="addTodo" class="px-4 py-2 ml-2 bg-blue-500 text-white rounded-lg">
+          <i class="fas fa-plus"></i> <!-- Font Awesome Add Icon -->
+        </button>
+      </div>
+      <ul class="space-y-2">
+        <li v-for="(todo, index) in todos" :key="index" :class="{ 'line-through': todo.completed }" class="bg-gray-100 p-2 rounded-lg">
+          <span>
+            <i @click="toggleCompleted(index)" :class="['fas', todo.completed ? 'fa-check-circle' : 'fa-circle', 'mr-2', 'cursor-pointer']"></i>
+            <i @click="deleteTodo(index)" class="fas fa-trash-alt"></i>
+            {{ todo.text }}
+          </span>
+        </li>
+      </ul>
+    </div>
+  `,
+});
+
+// Create the Vue instance
+new Vue({
+  el: '#app',
+  template: `<todo-list></todo-list>`,
 });
